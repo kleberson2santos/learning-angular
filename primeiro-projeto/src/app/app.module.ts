1c20737e2c59aaafcba9aaf7ed69d09294f5330a
Kleber <kleberson2santos@gmail.com>
2018-03-09 16:17:52 -0400

    first component genereted

---
+++ primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
