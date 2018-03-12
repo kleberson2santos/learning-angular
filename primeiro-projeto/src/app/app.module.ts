ddb32d8d3834c9e9eae3c3915d73db9742de060c
Kleber <kleberson2santos@gmail.com>
2018-03-12 17:57:08 -0400

    module and services

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
-  declarations: [
+  declarations: [ //Components Diretivas e Pipes
     AppComponent,
     MeuPrimeiroComponent,
     MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //GuardaDeRotas
+  bootstrap: [AppComponent] //ComponentPrincipal
 })
 export class AppModule { }
