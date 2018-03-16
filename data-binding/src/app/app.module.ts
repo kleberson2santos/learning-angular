7e7299259f0fa42b630fea295da92125df1b15a5
Kleber <kleberson2santos@gmail.com>
2018-03-16 09:55:03 -0400

    life-cycle of components

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { InputPropertyComponent } from './input-property/input-property.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
+import { CicloComponent } from './ciclo/ciclo.component';
 
 
 
@@ -16,7 +17,8 @@ import { OutputPropertyComponent } from './output-property/output-property.compo
     AppComponent,
     DataBindingComponent,
     InputPropertyComponent,
-    OutputPropertyComponent
+    OutputPropertyComponent,
+    CicloComponent
   ],
   imports: [
     BrowserModule,
