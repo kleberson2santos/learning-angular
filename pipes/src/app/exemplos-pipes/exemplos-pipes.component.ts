be546b735cf89027303e4c2ebddc1ab87864f4ee
Kleber <kleberson2santos@gmail.com>
2018-03-23 11:08:04 -0400

    pipes async with Promise and Observable

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -37,8 +39,12 @@ export class ExemplosPipesComponent implements OnInit {
       });
   }
 
-    
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000)
+  });
 
+  valorAsync2 = Observable.interval(2000)
+                          .map(valor => 'Valor assíncrono 2');
 
   constructor() { }
 
