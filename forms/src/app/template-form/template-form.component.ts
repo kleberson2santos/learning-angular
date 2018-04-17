25d33773a265252a204b6faa6e80f31565b0b6f2
Kleber <kleberson2santos@gmail.com>
2018-04-17 11:04:12 -0400

    searching address automatically with zip code

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-template-form',
@@ -9,10 +12,10 @@ export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
     nome: null,
-    email: 'kleberson@email.com'
+    email: null
   }
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
@@ -29,6 +32,19 @@ export class TemplateFormComponent implements OnInit {
       'has-error':this.verificaValidTouched(campo),
       'has-feedback':this.verificaValidTouched(campo)
     }
+  }
+
+  consultaCEP(cep){
+    cep = cep.replace(/\D/g, '');
+    if(cep != ""){
+      var validaCep = /^[0-9]{8}$/;
+      if(validaCep.test(cep)){
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+        .map(dados => dados.json())
+        .subscribe(dados => console.log(dados));
+      }
+    }
+   
   }
 
 }
